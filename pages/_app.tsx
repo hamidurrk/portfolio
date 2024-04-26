44c032b635531109c077128347be6987545edb55
hamidurrk <hamidurrk@gmail.com>
2024-04-27 00:35:21 +0600

    component fixed using resolution

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.scss";
-import type { AppProps } from "next/app";
+import React from 'react';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
