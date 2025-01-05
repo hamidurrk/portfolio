d9ccc2b020e0819222a1a0bff050325ad8630bbd
hamidurrk <hamidurrk@gmail.com>
2025-01-05 07:51:39 +0200

    refactor Audio component; remove unused code and simplify state management

--- components/common/Audio.tsx
+++ components/common/Audio.tsx
@@ -1,26 +1,12 @@
 "use client"; 
 
-import React, { useState, useRef, useEffect, createRef } from "react"; 
-import Button, { ButtonTypes } from "../common/button";
-import { PlayIcon, PauseIcon } from "lucide-react"; 
-import AudioPlayer from 'react-h5-audio-player';
+import React, { useState, useRef } from "react"; 
 import { isSmallScreen } from "pages";
 import styles from "./Audio.module.scss";
 
 const Audio = () => {
   const audioRef = useRef<HTMLAudioElement>(null);;
   const [isPlaying, setIsPlaying] = useState(false);
-
-  // useEffect(() => {
-  //   if (audioRef.current) {
-  //     audioRef.current.audio.current.src = "audio/something.mp3";
-  //     audioRef.current.audio.current.autoplay = false;
-  //     audioRef.current.audio.current.volume = 0.1;
-  //     audioRef.current.audio.current.loop = true;
-  //     audioRef.current.audio.current.muted = false;
-  //     audioRef.current.audio.current.preload = "none"; 
-  //   }
-  // }, []);
 
   const handlePlayPause = () => {
     if (audioRef.current) {
@@ -45,15 +31,6 @@ const Audio = () => {
   return (
     <div className={`${styles.audioContainer}`}
       style={{ backgroundColor: isSmallScreen() ? "transparent" : "#573217" }}>
-      {/* <AudioPlayer
-        ref={audioRef}
-        key={key}
-        src="audio/something.mp3"
-        autoPlay={false}
-        volume={0.5}
-        style={{ display: 'none' }} 
-      /> */}
-
       <ul className={`${styles.loaderList}`}>
         <li>
           <div 
