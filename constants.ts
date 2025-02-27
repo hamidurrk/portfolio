9fb6725718168d1f34a70c952cfd2e03052f4b5c
hamidurrk <hamidurrk@gmail.com>
2025-02-28 00:01:53 +0200

    refactor: update subtitle in timeline to clarify major focus on Machine Learning Models and AI

--- constants.ts
+++ constants.ts
@@ -447,7 +447,7 @@ export const TIMELINE: Array<TimelineNodeV2> = [
     title: "Started B.Sc. in Computational Engineering and Artificial Intelligence",
     size: ItemSize.SMALL,
     subtitle:
-      "Currently studying at LUT University in Finland. While my major focuses on ML Models and Natural Language Processing, my minor, software and systems engineering, covers the development of software systems.",
+      "Currently studying at LUT University in Finland. While my major focuses on Machine Learning Models and AI, my minor, software and systems engineering, covers the development of software systems.",
     image: "/timeline/lut_logo.png",
     slideImage: "/timeline/lut.jpg",
     shouldDrawLine: true,
